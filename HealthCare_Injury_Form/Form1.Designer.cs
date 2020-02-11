30ba3d7c3955a730531fca0d3cd72c11d5679c14
Ying Ye <guduyuan@gmail.com>
2020-02-11 10:17:50 -0600

    Create login form and set up database for user verifying

---
+++ HealthCare_Injury_Form/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HealthCare_Injury_Form
+{
+    partial class fmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnLgn = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(145, 125);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(60, 13);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "User Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(272, 117);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(272, 162);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 3;
+            // 
+            // btnLgn
+            // 
+            this.btnLgn.Location = new System.Drawing.Point(272, 224);
+            this.btnLgn.Name = "btnLgn";
+            this.btnLgn.Size = new System.Drawing.Size(50, 23);
+            this.btnLgn.TabIndex = 4;
+            this.btnLgn.Text = "Login";
+            this.btnLgn.UseVisualStyleBackColor = true;
+            this.btnLgn.Click += new System.EventHandler(this.btnLgn_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(342, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(49, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLgn);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblUser);
+            this.Name = "fmLogin";
+            this.Text = "Login Page";
+            this.Load += new System.EventHandler(this.fmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnLgn;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ HealthCare_Injury_Form/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthCare_Injury_Form
+{
+    public partial class fmLogin : Form
+    {
+        public fmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void fmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLgn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\user2\source\repos\HealthCare_Injury_Form\HealthCare_Injury_Form\Database1.mdf; Integrated Security = True");
+            SqlDataAdapter sda = new SqlDataAdapter("select count(*) from Login where username=")
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
