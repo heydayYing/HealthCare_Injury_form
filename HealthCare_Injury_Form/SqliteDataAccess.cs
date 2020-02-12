e85e00dd7d8e046111b611fbc5f18b8658c94aef
Ying Ye <guduyuan@gmail.com>
2020-02-12 16:17:15 -0600

    complete login form

--- HealthCare_Injury_Form/SqliteDataAccess.cs
+++ HealthCare_Injury_Form/SqliteDataAccess.cs
@@ -6,7 +6,7 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Dapper;
 
 namespace HealthCare_Injury_Form
 {
@@ -22,14 +22,19 @@ namespace HealthCare_Injury_Form
         {
             string cnn = LoadConnectionString();
             database = new SQLiteConnection(cnn);
+            database.Open();
             
         }
 
-        public bool verifyUser(string userName, string pwd)
+        public bool verifyUser(string name, string pswd)
         {
-            //var userItem = database.c
-            //return userItem != null;
-            return false;
+            
+                var userItem = database.Query<User>("Select * from User where userName = '"+name+"'"+" and pwd = '"+pswd+"'", new DynamicParameters());
+                
+                return userItem.Count() != 0;
+            
+           
+           
         }
 
     }
--- HealthCare_Injury_Form/User.cs
+++ HealthCare_Injury_Form/User.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HealthCare_Injury_Form
 {
     public class User
     {
-      
+       
         private int UserID { get; set; }
-    
+       
         public string userName { get; set; }
-        
+       
         public string pwd { get; set; }
     }
 }
